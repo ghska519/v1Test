0872f67e9fd0919dac7c23db1a76cc204d236e12
DESKTOP-E8959V0\nssys <ghska519@163.com>
2023-06-19 12:17:01 +0900

    ckeditor 추가

---
+++ 참고자료/ckeditor/ckeditor/config.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	
+	config.height = 300;
+	config.toolbarCanCollapse = true;
+	config.font_names = '맑은 고딕/Malgun Gothic;굴림/Gulim;돋움/Dotum;바탕/Batang;궁서/Gungsuh;' + config.font_names;
+	
+	config.filebrowserUploadUrl = '/editor/upload.php';
+};
